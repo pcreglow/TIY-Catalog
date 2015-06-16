6437abfcfd54bd4099b7d5d39a0928c0c0b0a992
Phillip Creglow <phillipcreglow@msn.com>
2015-06-16 16:51:08 -0400

    Fixed gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('do-something', function(){
-  console.log(arguments);
-  console.log('I did something!');
+gulp.task('serve',['sass'], function(){
+  browserSync.init({
+    server: "src/"
+  });
 
+  gulp.watch("src/scss/*.scss", ['sass']);
+  gulp.watch("src/*.html").on('change', browserSync.reload), [ 'build' ];
+  gulp.watch("src/js/**/*.js").on('change', browserSync.reload), ['build'];
 });
 
 gulp.task('sass', function(){
-  /*var sass = require('node-sass');
-
-  sass.render({
-    file: 'src/scss/main.scss',
-    outFile: 'src/css/main.css'
-
-  });*/
 
   var sass = require('gulp-sass');
 
@@ -22,34 +20,22 @@ gulp.task('sass', function(){
     .pipe(gulp.dest('src/css/'))
     .pipe(sass({outputStyle: 'compressed' }))
     .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream())
+
 });
 
-gulp.task('watch', function(){
-  gulp.watch('src/scss/*.scss', [ 'sass' ], function(){
-    console.log('They changed something..');
-  });
-  gulp.watch('src/*.html', [ 'build' ]);
-})
+
 
 gulp.task('clean', function(done){
   var del = require('del');
   del([
-    'dist/**/*',
+    'dist/**/*.*',
     'dist/**/.*',
+    'dist/*.*',
+    'dist/*',
     '!dist/.gitignore'
   ], done);
 });
-
-/*gulp.task('browser', function(){
- var browser = require('browser-sync');
- browser.init({
-         server: {
-             baseDir: "src/"
-         }
-     });
-     gulp.watch('src/scss/*.scss', ['sass']);
-   gulp.watch('src/*.html').on('change', browserSync.reload);
-});*/
 
 gulp.task('build', [ 'clean', 'sass' ], function(){
   gulp.src([
